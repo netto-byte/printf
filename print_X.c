09e2e4025094efe5b5ece854af5a26553d64b0e9
netto-byte <marvellousmatthew@gmail.com>
2023-12-08 08:23:13 -0800

    Function to convert and print a base 10 in base 16 (uppercase)

---
+++ print_X.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_X - Converts and prints a base 10 value in base 16 (Uppercase)
+ * @a_ptr: An argument pointer
+ * @buffer: Buffer for formatting
+ *
+ * Return: Number of characters printed.
+ */
+int print_X(va_list a_ptr, char buffer[])
+{
+	char upperHex[] = "0123456789ABCDEF";
+	unsigned int hex = va_arg(a_ptr, unsigned int);
+	int i = BUF_SIZE - 2, count = 0, p;
+
+	buffer[BUF_SIZE - 1] = '\0';
+
+	if (hex == 0)
+		buffer[i--] = '0';
+
+	while (hex > 0)
+	{
+		buffer[i--] = upperHex[hex % 16];
+		hex /= 16;
+	}
+	buffer[i--] = 'x';
+	buffer[i] = '0';
+
+	for (p = i; buffer[p]; p++)
+		count += write(1, &buffer[p], 1);
+	return (count);
+}
